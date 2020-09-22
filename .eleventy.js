67bb3c1a7f9a9876db5e0d15cc438df0a039268a
Rowdy Rabouw <rowdy@double-r.nl>
2020-09-22 14:48:00 +0200

    improvement: adopted personal preferences from other projects

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,14 @@
-const collections = require("./utils/collections.js");
-const filters = require("./utils/filters.js");
-const transforms = require("./utils/transforms.js");
+const collections = require('./utils/collections.js');
+const filters = require('./utils/filters.js');
+const transforms = require('./utils/transforms.js');
+const ErrorOverlay = require('eleventy-plugin-error-overlay');
 
 module.exports = function (eleventyConfig) {
 	// folders to copy to build dir
-	eleventyConfig.addPassthroughCopy("src/static");
+	eleventyConfig.addPassthroughCopy('src/static');
+
+	// shows 11ty error name, message and stacktrace in the browser
+	eleventyConfig.addPlugin(ErrorOverlay);
 
 	// collections
 	Object.keys(collections).forEach((collectionName) => {
@@ -26,13 +30,14 @@ module.exports = function (eleventyConfig) {
 
 	return {
 		dir: {
-			input: "src/",
-			output: "dist",
-			includes: "_includes",
-			layouts: "_layouts",
+			input: 'src',
+			output: 'dist',
+			includes: '_templates',
+			layouts: '_layouts',
+			data: '_data',
 		},
-		templateFormats: ["html", "md", "njk"],
-		htmlTemplateEngine: "njk",
+		templateFormats: ['html', 'md', 'njk'],
+		htmlTemplateEngine: 'njk',
 
 		// enable eleventy to pass dirs specified above
 		passthroughFileCopy: true,

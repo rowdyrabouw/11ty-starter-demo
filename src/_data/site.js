2e4c7851ccec8d50198628f656cb986177d90eb1
Rowdy Rabouw <rowdy@double-r.nl>
2020-09-22 19:57:25 +0200

    feat: update demo page

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,10 +1,10 @@
 module.exports = {
 	title: '11ty-starter',
 	description: 'Minimal boilerplate for new projects built with Eleventy, Tailwind, PurgeCSS and PostCSS',
-	url: 'https://rowdy.codes',
+	url: 'https://github.com/rowdyrabouw/11ty-starter',
 	author: 'Rowdy Rabouw',
 	meta_data: {
 		twitter: '@rowdyrabouw',
-		default_social_image: '/static/img/default_social_image.jpg',
+		default_social_image: '/img/default_social_image.jpg',
 	},
 };
